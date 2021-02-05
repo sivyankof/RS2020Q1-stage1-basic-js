b3280505a26e810babe6519a0cac9fde600a8594
Artem Sivyankov <morfiy0393@gmail.com>
2021-02-05 13:06:48 +0300

    init: task cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+    if (
+        sampleActivity < 0 ||
+        sampleActivity > 15 ||
+        typeof sampleActivity == Number
+    ) {
+        return false;
+    } else {
+        let result =
+            Math.log(MODERN_ACTIVITY / sampleActivity) /
+            (0.693 / HALF_LIFE_PERIOD);
+        console.log(Math.ceil(result));
+    }
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arg) {
+    let sum = 0;
+    let arr = arg.flat();
+
+    arr = arr.map((e) => {
+
+        if (e == "^^" || e == "^^") {
+            sum += 1;
+        }
+    });
+
+    return sum;
 };
+
