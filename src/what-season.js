d7d6526489591f1c5fb5e3e1682d47ef38792829
Artem Sivyankov <morfiy0393@gmail.com>
2021-02-05 16:33:00 +0300

    feat: add task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    const season = {
+        winter: [11, 0, 1],
+        spring: [2, 3, 4],
+        summer: [5, 6, 7],
+        autumn: [8, 9, 10],
+    };
+
+    if (!date) {
+        return "Unable to determine the time of year!";
+    }
+    if (Object.prototype.toString.call(date) !== "[object Date]") {
+        throw new Error();
+    }
+
+    for (let key in season) {
+        if (season[key].indexOf(date.getMonth()) !== -1) {
+            return key;
+        }
+    }
 };
