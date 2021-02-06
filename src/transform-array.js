5aaf8d896c9e461cfcddcea6e1306e804e8e65b8
Artem Sivyankov <morfiy0393@gmail.com>
2021-02-06 17:16:52 +0300

    init task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,31 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+    let array = false;
+    if (arr.length == 0) return [];
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == "--discard-next") {
+            i++;
+            array = true;
+        } else if (arr[i] == "--discard-prev") {
+            if (i != 0 && arr[i - 2] != "--discard-next") result.pop();
+            array = true;
+        } else if (arr[i] == "--double-next") {
+            if (i < arr.length - 1) {
+                result.push(arr[i + 1]);
+            }
+            array = true;
+        } else if (arr[i] == "--double-prev") {
+            if (i != 0 && arr[i - 2] != "--discard-next") {
+                result.push(arr[i - 1]);
+            }
+            array = true;
+        } else {
+            result.push(arr[i]);
+        }
+    }
+    if (array) return result;
+    else return arr;
 };
